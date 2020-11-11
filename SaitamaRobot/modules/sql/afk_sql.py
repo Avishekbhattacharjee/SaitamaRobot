cb8ffa4257c799789ef026e42238f3ba69a8c0dc
Avishekbhattacharjee <52344359+Avishekbhattacharjee@users.noreply.github.com>
2020-11-11 12:32:06 +0530

    Update afk_sql.py

--- SaitamaRobot/modules/sql/afk_sql.py
+++ SaitamaRobot/modules/sql/afk_sql.py
@@ -3,7 +3,6 @@ import threading
 from SaitamaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 
-
 class AFK(BASE):
     __tablename__ = "afk_users"
 
@@ -83,9 +82,7 @@ def __load_afk_users():
     global AFK_USERS
     try:
         all_afk = SESSION.query(AFK).all()
-        AFK_USERS = {
-            user.user_id: user.reason for user in all_afk if user.is_afk
-        }
+        AFK_USERS = {user.user_id: user.reason for user in all_afk if user.is_afk}
     finally:
         SESSION.close()
 
